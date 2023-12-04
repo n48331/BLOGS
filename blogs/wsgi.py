2ccd44581506e13a65625b956bc165a7bc1bf3d7
n48331 <unfolks@gmail.com>
2023-12-05 00:28:21 +0530

    initial

--- blogs/wsgi.py
+++ blogs/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for youser project.
+WSGI config for blogs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youser.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogs.settings')
 
 application = get_wsgi_application()
